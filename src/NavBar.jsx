0c2d5b0d28522d63578e63ab90decd0f6af79833
unknown <gustavosecardovelli@gmail.com>
2024-05-13 17:04:10 -0300

    feat: react-router is added

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,13 +1,17 @@
 function NavBar() {
+    const homeLink = ()=>{window.location.href = 'home'}
+    const pageLink = ()=>{window.location.href = '/'}
+    const loginLink = ()=>{window.location.href = 'login'}
+
     return (
         <nav className="bgs-navbar">
-            <div className="bgs-nav-item">
+            <div className="bgs-nav-item" onClick={homeLink}>
                 <h1>BLOGGERSS</h1>
             </div>
-            <div className="bgs-nav-item nav-mid">
+            <div className="bgs-nav-item nav-mid" onClick={pageLink}>
                 <h1 >GSSS</h1>
             </div>
-            <div className="bgs-nav-item">
+            <div className="bgs-nav-item" onClick={loginLink}>
                 <h1>LOGIN</h1>
             </div>
         </nav>
