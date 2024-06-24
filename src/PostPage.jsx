1bbd76829507888e1823b57398801336921d4c66
Gustavo <gustavosecardovelli@gmail.com>
2024-06-24 00:49:13 -0300

    feat: style att

--- src/PostPage.jsx
+++ src/PostPage.jsx
@@ -26,7 +26,7 @@ const PostPage = () => {
                     <h1>Post Title</h1>
                     <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis ea repudiandae libero soluta mollitia accusamus quasi et! Recusandae consequatur, qui saepe doloribus quibusdam impedit sed possimus sunt eaque voluptatum? Repudiandae.</span>
                     <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas laudantium amet repellendus fuga voluptate sequi inventore repudiandae sint consequatur tempore repellat labore, debitis sed voluptatibus soluta illum similique dolorum! Reiciendis.</span>
-                    <img src="https://1.bp.blogspot.com/-p9bqe1PgXv8/Xn5B0oNVSnI/AAAAAAAApuU/82PtUVI1TO4BVizryZGxWb3BaQby0yxeACLcBGAsYHQ/s2560/mountains_lake_spruce_164558_2560x1440.jpg" alt="" />
+                    {/* <img src="https://1.bp.blogspot.com/-p9bqe1PgXv8/Xn5B0oNVSnI/AAAAAAAApuU/82PtUVI1TO4BVizryZGxWb3BaQby0yxeACLcBGAsYHQ/s2560/mountains_lake_spruce_164558_2560x1440.jpg" alt="" /> */}
                     <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex id porro sed deserunt recusandae minus? Sit sapiente porro aut reiciendis non fugit quod, corrupti tempora perspiciatis, eveniet laudantium esse maxime!</span>
 
                 </div>
